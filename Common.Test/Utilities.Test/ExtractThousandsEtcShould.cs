96a21011f105b77c55e6842e1dc82424b0280b00
Oscar Johander <oscar@fripost.org>
2019-12-18 23:30:03 +0100

    Add utility class to Common with int extension method to return thousands, hundreds etc from int.

---
+++ Common.Test/Utilities.Test/ExtractThousandsEtcShould.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Common;
+
+namespace Common.Test.Utilities.Test
+{
+    class ExtractThousandsEtcShould
+    {
+        [TestCase(0, 0, 0, 0, 0)]
+        [TestCase(1, 0, 0, 0, 1)]
+        [TestCase(5, 0, 0, 0, 5)]
+        [TestCase(10, 0, 0, 1, 0)]
+        [TestCase(11, 0, 0, 1, 1)]
+        [TestCase(100, 0, 1, 0, 0)]
+        [TestCase(101, 0, 1, 0, 1)]
+        [TestCase(111, 0, 1, 1, 1)]
+        [TestCase(1000, 1, 0, 0, 0)]
+        [TestCase(1111, 1, 1, 1, 1)]
+        [TestCase(1010, 1, 0, 1, 0)]
+        [TestCase(1002, 1, 0, 0, 2)]
+        [TestCase(9999, 9, 9, 9, 9)]
+        [TestCase(999, 0, 9, 9, 9)]
+        [TestCase(99, 0, 0, 9, 9)]
+        public void ReturnCorrectValuesForInputsLessThan9999(int input, int thousands, int hundreds, int tens, int ones)
+        {
+            var (Thousands, Hundreds, Tens, Ones) = input.ExtractThousandsHundredsTensAndOnes();
+
+            Assert.That(Thousands, Is.EqualTo(thousands));
+            Assert.That(Hundreds, Is.EqualTo(hundreds));
+            Assert.That(Tens, Is.EqualTo(tens));
+            Assert.That(Ones, Is.EqualTo(ones));
+        }
+
+        [TestCase(-1)]
+        [TestCase(10_000)]
+        public void ThrowExceptionsForIntsOutOfRange(int input)
+        {
+            Assert.That(() => input.ExtractThousandsHundredsTensAndOnes(), Throws.ArgumentException);
+
+        }
+    }
+}
---
+++ Common/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class Utilities
+    {
+        public static (int Thousands, int Hundreds, int Tens, int Ones) ExtractThousandsHundredsTensAndOnes(this int integer)
+        {
+            if(integer < 0 || integer > 9_999)
+                throw new ArgumentException();
+
+            int[] results = new[] { 0, 0, 0, 0 };
+
+            int index = 0;
+            decimal x = integer;
+            while(x >= 1)
+            {
+                x = x / 10;
+                decimal intPart = Math.Truncate(x);
+                results[index++] = (int)((x - intPart) * 10);
+                x = intPart;
+            }
+            return (results[3], results[2], results[1], results[0]);
+        }
+    }
+}
