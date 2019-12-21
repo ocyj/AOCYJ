43515b562ba064330a000a7d1782a6e8058c49a3
Oscar Johander <oscar@fripost.org>
2019-12-21 16:23:29 +0100

    Add solution to day 6.

--- AOC2019/Day06.cs
+++ AOC2019/Day06.cs
@@ -1,24 +1,62 @@
-﻿using Common;
+﻿using AOC2019.D06;
+using Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AOC2019
 {
     class Day06 : Day
     {
+        private Dictionary<string, SpaceObject> spaceObjects;
+
         public override int Date => 6;
 
         public override string Name => "Universal Orbit Map";
 
+        public Day06()
+        {
+            spaceObjects = ParseInput();
+        }
+
         public override object SolvePart1()
         {
-            return "";
+            return $"Total orbits {spaceObjects.Sum(kvp => kvp.Value.TotalOrbits()).ToString()}";
         }
 
         public override object SolvePart2()
         {
-            return "";
+            var myWayToCom = spaceObjects["YOU"].WayToCom.ToHashSet();
+            var santasWayToCom = spaceObjects["SAN"].WayToCom.ToHashSet();
+            int shortestWay = myWayToCom.Count + santasWayToCom.Count - 2 * myWayToCom.Intersect(santasWayToCom).ToHashSet().Count;
+            return $"Shortest way to santa {shortestWay}";
+        }
+        private Dictionary<string, SpaceObject> ParseInput()
+        {
+            var spaceObjects = new Dictionary<string, SpaceObject>
+            {
+                { "COM", new SpaceObject("COM", null) }
+            };
+            foreach (var orbitPair in Input.Select(s => s.Split(')')))
+            {
+                // The orbitor is in orbit around the orbitee
+                string orbitorName = orbitPair[0];
+                string orbiteeName = orbitPair[1];
+                if (!spaceObjects.TryGetValue(orbitorName, out SpaceObject orbitor))
+                {
+                    orbitor = new SpaceObject(orbitorName);
+                    spaceObjects.Add(orbitorName, orbitor);
+                }
+                if (!spaceObjects.TryGetValue(orbiteeName, out SpaceObject orbitee))
+                {
+                    orbitee = new SpaceObject(orbiteeName);
+                    spaceObjects.Add(orbiteeName, orbitee);
+                }
+
+                orbitee.OrbitsAround = orbitor;
+            }
+            return spaceObjects;
         }
     }
 }
