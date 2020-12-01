bd9639310fbc8da954cd967377be7aa460dae781
Oscar Johander <ocyj@protonmail.com>
2020-12-01 08:26:14 +0100

    Refactor solver to shared project

---
+++ CommonShared/Solver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Common
+{
+    public static class Solver
+    {
+        public static void Solve()
+        {
+            Console.WriteLine();
+            var dayTypes = Assembly.GetExecutingAssembly().GetTypes()
+                                   .Where(t => t.BaseType == typeof(Day));
+            foreach (var dayType in dayTypes)
+            {
+                Day day = (Day)Activator.CreateInstance(dayType);
+                day.Solve();
+            }
+        }
+    }
+}
