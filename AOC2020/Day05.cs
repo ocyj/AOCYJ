81522cc0940ad30111d1ae98446b2bbef17c663a
Oscar Johander <ocyj@protonmail.com>
2020-12-05 20:08:59 +0100

    Change formatting, update varibale names.

--- AOC2020/Day05.cs
+++ AOC2020/Day05.cs
@@ -19,18 +19,20 @@ namespace AOC2020
 
         public override object SolvePart1() => BoardingPasses.Max();
 
-        public override object SolvePart2() => Enumerable.Range(BoardingPasses.Min(), BoardingPasses.Max() - BoardingPasses.Min() + 1).Except(BoardingPasses).Single();
+        public override object SolvePart2() =>
+            Enumerable.Range(BoardingPasses.Min(), BoardingPasses.Max() - BoardingPasses.Min() + 1)
+            .Except(BoardingPasses).Single();
 
         internal override void Prepare()
         {
             BoardingPasses = Input.Select(boardingPass => FindSeatId(boardingPass)).ToList();
         }
 
-        private static (int Lo, int Hi) FindNextSplit(string p, (int Lo, int Hi) bounds)
+        private static (int Lo, int Hi) FindNextSplit(string splitChoice, (int Lo, int Hi) bounds)
         {
-            p = p.Replace('L', 'F').Replace('R', 'B');
+            splitChoice = splitChoice.Replace('L', 'F').Replace('R', 'B');
             int mid = ((bounds.Hi - bounds.Lo) >> 1) + bounds.Lo;
-            return p switch
+            return splitChoice switch
             {
                 "F" => (bounds.Lo, mid),
                 "B" => (mid + 1, bounds.Hi),
