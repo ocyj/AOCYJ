bd49caeb07e11f3b27e10c921f581a1029a5f44c
Oscar Johander <ocyj@protonmail.com>
2020-12-06 13:30:21 +0100

    WIP: Add 2020 day 6 solution.

---
+++ AOC2020/Day06.cs
@@ -0,0 +1,50 @@
+﻿using CommonShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2020
+{
+    public class Day06 : Day
+    {
+        private List<HashSet<char>> CustomForms;
+
+        public override int Date => 6;
+
+        public override string Name => "Custom Customs";
+
+        public override object SolvePart1() => CustomForms.Sum(cf => cf.Count);
+
+        public override object SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Prepare()
+        {
+            UseTestInput = true;
+
+            TestInput = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b".Split(Environment.NewLine);
+
+
+         
+
+        }
+    }
+}
