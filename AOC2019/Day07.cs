5f8c78c6144faa3500cac95866867f4aece25fd3
Oscar Johander <ocyj@protonmail.com>
2021-10-11 23:59:05 +0200

    First attempt to solve part 2, not working.. try with test input.

--- AOC2019/Day07.cs
+++ AOC2019/Day07.cs
@@ -10,6 +10,8 @@ namespace AOC2019
 
         public override string Name => "Amplification Circuit";
 
+
+
         public override object SolvePart1()
         {
             int[] program = Input[0].Split(',').Select(s => int.Parse(s)).ToArray();
@@ -73,7 +75,76 @@ namespace AOC2019
 
         public override object SolvePart2()
         {
-            return "";
+            UseTestInput = true;
+            TestInput = new[] { "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5" };
+            int[] program = Input[0].Split(',').Select(s => int.Parse(s)).ToArray();
+
+            int maxOutput = int.MinValue;
+            int[] phaseSettingsMax = null;
+            foreach (var permutatation in (new int[] { 5, 6, 7, 8, 9 }).AllPermutations())
+            {
+                var computerA = new IntCodeComputer(program);
+                var computerB = new IntCodeComputer(program);
+                var computerC = new IntCodeComputer(program);
+                var computerD = new IntCodeComputer(program);
+                var computerE = new IntCodeComputer(program);
+
+
+                var cnxAB = new Connection(permutatation[1]);
+                computerA.WriteOutput = cnxAB.WriteValue;
+                computerB.ReadInput = cnxAB.ReadValue;
+
+                var cnxBC = new Connection(permutatation[2]);
+                computerB.WriteOutput = cnxBC.WriteValue;
+                computerC.ReadInput = cnxBC.ReadValue;
+
+                var cnxCD = new Connection(permutatation[3]);
+                computerC.WriteOutput = cnxCD.WriteValue;
+                computerD.ReadInput = cnxCD.ReadValue;
+
+                var cnxDE = new Connection(permutatation[4]);
+                computerD.WriteOutput = cnxDE.WriteValue;
+                computerE.ReadInput = cnxDE.ReadValue;
+
+                var cnxEA = new Connection(permutatation[0]);
+
+                int compAReadNo = 0;
+                computerA.ReadInput = () =>
+                {
+                    if (compAReadNo++ == 0)
+                    {
+                        //return cnxEA.ReadValue();
+                        return Task.FromResult(0);
+                    }
+                    else if(compAReadNo++ == 1)
+                    {
+                        //return Task.FromResult(0);
+                        return cnxEA.ReadValue();
+                    }
+                    else
+                    {
+                        return cnxEA.ReadValue();
+                    }
+                };
+
+                computerE.WriteOutput = cnxEA.WriteValue;
+
+                var taskA = computerA.RunToCompletion();
+                var taskB = computerB.RunToCompletion();
+                var taskC = computerC.RunToCompletion();
+                var taskD = computerD.RunToCompletion();
+                var taskE = computerE.RunToCompletion();
+
+                Task.WhenAll(taskA, taskB, taskC, taskD, taskE);
+
+                int thrusterSignal = cnxEA.Value;
+                if (thrusterSignal > maxOutput)
+                {
+                    maxOutput = thrusterSignal;
+                    phaseSettingsMax = permutatation;
+                }
+            }
+            return $"Maximum output was {maxOutput}";
         }
     }
 
